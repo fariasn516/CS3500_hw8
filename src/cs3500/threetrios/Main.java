a4e632806c849d09b7aae6028f450fa86c6b7639
fariasn516 <145229665+fariasn516@users.noreply.github.com>
2024-12-03 18:32:57 -0500

    implemented AI player

--- src/cs3500/threetrios/Main.java
+++ src/cs3500/threetrios/Main.java
@@ -2,6 +2,7 @@ package cs3500.threetrios;
 
 import javax.naming.ldap.Control;
 
+import cs3500.threetrios.adapter.AIPlayerAdapter;
 import cs3500.threetrios.adapter.ControllerAdapter;
 import cs3500.threetrios.adapter.GridAdapter;
 import cs3500.threetrios.adapter.HumanPlayerAdapter;
@@ -33,9 +34,12 @@ public class Main {
    * @param args represents a String of inputs from the console
    */
   public static void main(String[] args) {
-    /**Model model = new ThreeTriosModel();
-    ThreeTriosModelView viewPlayer1 = new ThreeTriosModelView(model, Color.RED);
-    ThreeTriosModelView viewPlayer2 = new ThreeTriosModelView(model, Color.BLUE);
+
+    ModelAdapter model = new ModelAdapter();
+    ThreeTriosFrameView viewPlayer1 = new ThreeTriosModelView(model, Color.RED);
+    ThreeTriosFrameView viewPlayer2 = new ViewAdapter(model, "BLUE");
+
+    IGrid grid = new GridAdapter();
 
     if (args.length != 2) {
       throw new IllegalArgumentException("Please specify players.");
@@ -45,46 +49,39 @@ public class Main {
     Player player2 = null;
 
     if (args[0].equalsIgnoreCase("human")) {
-      player1 = new HumanPlayer(model, Color.RED);
-    } else if (args[0].equalsIgnoreCase("strategy1")) {
-      player1 = new AIPlayer(model, Color.RED, new MaxFlippedCardsStrat());
-    } else if (args[0].equalsIgnoreCase("strategy2")) {
-      player1 = new AIPlayer(model, Color.RED, new CornerCardStrat());
-    } else {
+      player1 = new HumanPlayerAdapter(model, Color.RED);
+    }
+    else if (args[0].equalsIgnoreCase("strategy1")) {
+      player1 = new AIPlayerAdapter(model, Color.RED, new MaxFlippedCardsStrat());
+    }
+    else if (args[0].equalsIgnoreCase("strategy2")) {
+      player1 = new AIPlayerAdapter(model, Color.RED, new CornerCardStrat());
+    }
+    else {
       throw new IllegalArgumentException("Invalid player type for player 1. " +
               "Use 'human' or 'strategyX'.");
     }
 
     if (args[1].equalsIgnoreCase("human")) {
-      player2 = new HumanPlayer(model, Color.BLUE);
-    } else if (args[1].equalsIgnoreCase("strategy1")) {
-      player2 = new AIPlayer(model, Color.BLUE, new MaxFlippedCardsStrat());
-    } else if (args[1].equalsIgnoreCase("strategy2")) {
-      player2 = new AIPlayer(model, Color.BLUE, new CornerCardStrat());
-    } else {
+      player2 = new HumanPlayerAdapter(model, Color.BLUE);
+    }
+    else if (args[1].equalsIgnoreCase("strategy1")) {
+      player2 = new AIPlayerAdapter(model, Color.BLUE, new MaxFlippedCardsStrat());
+    }
+    else if (args[1].equalsIgnoreCase("strategy2")) {
+      player2 = new AIPlayerAdapter(model, Color.BLUE, new CornerCardStrat());
+    }
+    else {
       throw new IllegalArgumentException("Invalid player type for player 2. " +
               "Use 'human' or 'strategyX'.");
     }
-    
+
     PlayerController controller1 = new ThreeTriosPlayerController(model, player1, viewPlayer1);
+    ControllerAdapter controller2 = new ControllerAdapter(model, player2, (ViewAdapter)viewPlayer2);
+
     controller1.startGame("configurationFiles/GridConfiguration/HasHoles",
             "configurationFiles/CardConfiguration/MaxCards", false);
-    PlayerController controller2 = new ThreeTriosPlayerController(model, player2, viewPlayer2);
-    controller2.startGame("configurationFiles/GridConfiguration/HasHoles",
-            "configurationFiles/CardConfiguration/MaxCards", false);**/
-
-    ModelAdapter model2 = new ModelAdapter();
-    IGrid grid = new GridAdapter();
-
-    Player player3 = new HumanPlayerAdapter(model2, Color.RED);
-    Player player4 = new HumanPlayerAdapter(model2, Color.BLUE);
-    ThreeTriosFrameView viewPlayer3 = new ThreeTriosModelView(model2, Color.RED);
-    ThreeTriosFrameView viewPlayer4 = new ViewAdapter(model2, "BLUE");
-    PlayerController controller3 = new ThreeTriosPlayerController(model2, player3, viewPlayer3);
-    ControllerAdapter controller4 = new ControllerAdapter(model2, player4, (ViewAdapter)viewPlayer4);
-    controller3.startGame("configurationFiles/GridConfiguration/HasHoles",
-            "configurationFiles/CardConfiguration/MaxCards", false);
-    controller4.playGame("configurationFiles/CardConfiguration/MaxCards",
-            "configurationFiles/GridConfiguration/HasHoles", model2, grid);
+    controller2.playGame("configurationFiles/CardConfiguration/MaxCards",
+            "configurationFiles/GridConfiguration/HasHoles", model, grid);
   }
 }
--- src/cs3500/threetrios/adapter/AIPlayerAdapter.java
+++ src/cs3500/threetrios/adapter/AIPlayerAdapter.java
@@ -1,9 +1,16 @@
 package cs3500.threetrios.adapter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import cs3500.threetrios.model.Card;
 import cs3500.threetrios.model.Color;
+import cs3500.threetrios.model.Direction;
 import cs3500.threetrios.model.Model;
+import cs3500.threetrios.model.SimpleCard;
+import cs3500.threetrios.model.Value;
 import cs3500.threetrios.model.player.AIPlayer;
 import cs3500.threetrios.provider.hw5.CardColor;
 import cs3500.threetrios.provider.hw5.ICard;
@@ -30,17 +37,54 @@ public class AIPlayerAdapter extends AIPlayer implements IPlayer {
 
   @Override
   public void setHand(List<ICard> cards) throws IllegalArgumentException {
-
+    for (ICard card : cards) {
+        this.addToHand(generateCardFromICard(card));
+    }
+  }
+  private Card generateCardFromICard(ICard card) {
+    Map<Direction, Value> values = new HashMap<>();
+    for (Value val : Value.values()) {
+      if (card.getNorthValue() == val.getValue()) {
+        values.put(cs3500.threetrios.model.Direction.NORTH, val);
+      }
+      if (card.getSouthValue() == val.getValue()) {
+        values.put(cs3500.threetrios.model.Direction.SOUTH, val);
+      }
+      if (card.getEastValue() == val.getValue()) {
+        values.put(cs3500.threetrios.model.Direction.EAST, val);
+      }
+      if (card.getWestValue() == val.getValue()) {
+        values.put(cs3500.threetrios.model.Direction.WEST, val);
+      }
+    }
+    return new SimpleCard("", values);
   }
 
   @Override
   public List<ICard> getHand() {
-    return List.of();
+    List<ICard> icardList = new ArrayList<>();
+    for (Card card : this.getCardsInHand()) {
+      Map<cs3500.threetrios.provider.hw5.Direction, Integer> values = createICardMap(card);
+      ICard currCard = new CardAdapter(card.getName(), this.getColor2(), values);
+      icardList.add(currCard);
+    }
+    return icardList;
+  }
+
+  private Map<cs3500.threetrios.provider.hw5.Direction, Integer> createICardMap(Card card) {
+    Map<cs3500.threetrios.provider.hw5.Direction, Integer> values = new HashMap<>();
+    values.put(cs3500.threetrios.provider.hw5.Direction.NORTH, card.getValueFromDirection(cs3500.threetrios.model.Direction.NORTH));
+    values.put(cs3500.threetrios.provider.hw5.Direction.SOUTH, card.getValueFromDirection(cs3500.threetrios.model.Direction.SOUTH));
+    values.put(cs3500.threetrios.provider.hw5.Direction.EAST, card.getValueFromDirection(cs3500.threetrios.model.Direction.EAST));
+    values.put(cs3500.threetrios.provider.hw5.Direction.WEST, card.getValueFromDirection(cs3500.threetrios.model.Direction.WEST));
+    return values;
   }
 
   @Override
   public ICard chooseCard(int cardIndex) throws IllegalStateException {
-    return null;
+    Card card = this.getCardsInHand().get(cardIndex);
+    Map<cs3500.threetrios.provider.hw5.Direction, Integer> values = createICardMap(card);
+    return new CardAdapter(card.getName(), this.getColor2(), values);
   }
 
   @Override
@@ -55,21 +99,24 @@ public class AIPlayerAdapter extends AIPlayer implements IPlayer {
 
   @Override
   public int getScore() {
-    return 0;
+    return this.getNumberCardsOwned();
   }
 
   @Override
   public CardColor getColor2() {
-    return null;
+    return this.getColor() == Color.RED ? CardColor.RED : CardColor.BLUE;
   }
 
   @Override
   public void addCard(ICard card) {
-
+    this.getCardsInHand().add(generateCardFromICard(card));
   }
 
   @Override
   public void removeCard(int cardIndex) throws IllegalArgumentException {
-
+    if (cardIndex < 0 || cardIndex >= this.getCardsInHand().size()) {
+      throw new IllegalArgumentException("Invalid card index.");
+    }
+    this.getCardsInHand().remove(cardIndex);
   }
 }
